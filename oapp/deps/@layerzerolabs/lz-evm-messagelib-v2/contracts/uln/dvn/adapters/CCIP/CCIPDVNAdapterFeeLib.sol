23649f76bc55bf5b6f4bf3f3820633df4514531f
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-01-17 20:01:02 +0200

    add oz and lz deps
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

---
+++ oapp/deps/@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/dvn/adapters/CCIP/CCIPDVNAdapterFeeLib.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LZBL-1.2
+
+pragma solidity ^0.8.20;
+
+import { DVNAdapterFeeLibBase } from "../DVNAdapterFeeLibBase.sol";
+
+contract CCIPDVNAdapterFeeLib is DVNAdapterFeeLibBase {}
